56995541072b7f8cd4062efc2ad817bcdd644635
priyadharshinikk <32954170+priyadharshinikk@users.noreply.github.com>
2017-10-21 12:11:33 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece021
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece021 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+  a=6
+  if(a%2==0):
+   print("the number is even")
+  else:
+     print("the number is odd")
+
+
+if __name__ == '__main__':
+    main()
